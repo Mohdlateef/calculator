45d9bf4715de79da7de372e1d773e33ba42b4be2
unknown <lateefahbaba123@gmail.com>
2024-02-06 18:22:23 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Calcualtor from "./components/Calculator";
+
+const App=()=>{
+    return(
+<div>
+<Calcualtor />
+</div>
+    )
+}
+
+export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const Calcualtor = () => {
+  let [number1, setNumber1] = useState("");
+  let [number2, setNumber2] = useState("");
+  let [Output, setOutput] = useState("");
+  function num1(e) {
+    let number1 = +e.target.value;
+    setNumber1(number1);
+    // console.log(number1);
+  }
+  function num2(e) {
+    let number2 = +e.target.value;
+    setNumber2(number2);
+  }
+  function cal(event) {
+    event.preventDefault();
+    let opration = event.target.innerText;
+    if (number1 !== "" && number2 !== "") {
+      console.log("test1");
+      if (opration == "+") {
+        let sum = number1 + number2;
+        setOutput(sum);
+      } else if (opration == "-") {
+        let sum = number1 - number2;
+        setOutput(sum);
+      } else if (opration == "*") {
+        let sum = number1 * number2;
+        setOutput(sum);
+      } else if (opration == "/") {
+        let sum = number1 / number2;
+        setOutput(sum);
+      }
+    } else {
+      setOutput("Error");
+    }
+  }
+
+  return (
+    <div>
+      <div>Calcualtor</div>
+      <form>
+        <input type="number" onKeyUp={num1}></input>
+        <input type="number" onKeyUp={num2}></input>
+        <button onClick={cal}>+</button>
+        <button onClick={cal}>-</button>
+        <button onClick={cal}>*</button>
+        <button onClick={cal}>/</button>
+      </form>
+      <div>{Output}</div>
+    </div>
+  );
+};
+
+export default Calcualtor;
